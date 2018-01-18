ef27891e34ba2f0d9bfb48a30067625e66cd1008
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-18 08:10:32 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,36 @@
+function forLoop(array){
+  for(let i=0; i<20; i++){
+    if(i===1){
+      array.push("I am 1 strange loop.");
+    }
+    else {
+      array.push("I am ${i+1} strange loops");
+    }
+  }
+  return array;
+}
+
+var a = [];
+forLoop(a);
+function printArray(array){
+  for(let i = 0; i<array.length; i++){
+    console.log(array[i]);
+  }
+}
+
+function whileLoop(number){
+  while(number > 0){
+    console.log(number);
+    number --;
+  }
+  return "done"
+}
+
+function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+function doWhileLoop(array){
+  do
+    {array.splice(array.length-1, 1)}  
+}
